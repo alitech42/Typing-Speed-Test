9b99213b3ce4759aa8fe49f20996e983bdc2423d
ValienteRitter <tagizade2006f@gmail.com>
2026-02-27 18:38:15 +0400

    Feat: Create useTyping hook for typing logic

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,5 +1,30 @@
 import { useState, useEffect } from "react";
 
+export function useTyping(text: string) {
+    const [typingIndex, setTypingIndex] = useState(0);
+    const [typingSequence, setTypingSequence] = useState(() => {
+        const splittedText = text.split("");
+        return splittedText.map((char) => ({ char, status: "undefined" }));
+    });
+
+    const updateStatus = (index: number, status: string) => {
+        setTypingSequence((prev) =>
+            prev.map((char, i) =>
+                i === index
+                    ? {
+                          ...char,
+                          status,
+                      }
+                    : char,
+            ),
+        );
+    };
+
+    const increaseIndex = () => setTypingIndex((prev) => prev + 1);
+
+    return { typingIndex, typingSequence, updateStatus, increaseIndex };
+}
+
 export function useTimer(initialTime: number) {
     const [time, setTime] = useState(initialTime);
 
@@ -13,10 +38,10 @@ export function useTimer(initialTime: number) {
                 return prev - 1;
             });
         }, 1000);
-        return () => clearInterval(intervalId)
+        return () => clearInterval(intervalId);
     }, []);
 
-    return {time};
+    return { time };
 }
 
 export function useWPM(
