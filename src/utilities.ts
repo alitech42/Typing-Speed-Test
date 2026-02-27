224696fd49db9e85cdf7e7d5ef699dd272f955a9
ValienteRitter <tagizade2006f@gmail.com>
2026-02-27 16:26:57 +0400

    Create: useWPM hook for calculating WPM and Accuracy

--- src/utilities.ts
+++ src/utilities.ts
@@ -8,3 +8,17 @@ export function useTimer(initialTime: number) {
     return {time, decreement}
 }
 
+export function useWPM(time: number, typingSequence: {char: string, status: string}[]) {
+    const minutes = (60 - time) / 60;
+    const correctTypes = typingSequence.filter(
+        ({ status }) => status === "correct",
+    ).length;
+    const falseTypes = typingSequence.filter(
+        ({ status }) => status === "false",
+    ).length;
+    const accuracy = (correctTypes / (correctTypes + falseTypes)) * 100;
+    const grossWPM = (correctTypes + falseTypes) / 5 / minutes;
+    const netWPM = grossWPM - falseTypes / minutes;
+
+    return {accuracy, grossWPM, netWPM}
+}
