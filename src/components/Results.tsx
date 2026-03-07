343cd7a0916c27f639d4cbf71312db68d13783eb
ValienteRitter <tagizade2006f@gmail.com>
2026-03-07 22:05:19 +0400

    Refactor: use toFixed(2) on values

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -13,8 +13,8 @@ export function Results({wpm, accuracy, correctTypes, falseTypes}: ResultsProps)
             <img src="src/assets/images/icon-completed.svg" />
             <h1 className="font-bold text-3xl text-center">Test Complete!</h1>
             <p className="text-center text-[hsl(240,3%,46%)]">Solid run. Keep pushing to beat your high score</p>
-            <ResultCard label="WPM" value={wpm} color="hsl(0,0%,100%)"/>
-            <ResultCard label="Accuracy" value={accuracy + '%'} color="hsl(354,63%,57%)"/>
+            <ResultCard label="WPM" value={wpm.toFixed(2)} color="hsl(0,0%,100%)"/>
+            <ResultCard label="Accuracy" value={accuracy.toFixed(2) + '%'} color="hsl(354,63%,57%)"/>
             <ResultCard label="Characters" value={correctTypes + '/' + falseTypes} color="hsl(140,63%,57%)"/>
         </div>
     );
