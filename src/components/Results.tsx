979ca4d94c3e36d75ed07b7d8dacfec011f10bc4
ValienteRitter <tagizade2006f@gmail.com>
2026-03-08 20:25:28 +0400

    Refactor: display only the integer part of WPM.

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -14,7 +14,7 @@ export function Results({wpm, accuracy, correctTypes, falseTypes}: ResultsProps)
             <h1 className="font-bold text-3xl text-center">Test Complete!</h1>
             <p className="text-center text-[hsl(240,3%,46%)]">Solid run. Keep pushing to beat your high score</p>
             <div className="flex flex-col gap-5 w-full sm:flex-row sm:w-[50%] stretch">
-                <ResultCard label="WPM" value={wpm.toFixed(2)} color="hsl(0,0%,100%)"/>
+                <ResultCard label="WPM" value={wpm.toFixed(0)} color="hsl(0,0%,100%)"/>
                 <ResultCard label="Accuracy" value={accuracy.toFixed(2) + '%'} color="hsl(354,63%,57%)"/>
                 <ResultCard label="Characters" value={correctTypes + '/' + falseTypes} color="hsl(140,63%,57%)"/>
             </div>
