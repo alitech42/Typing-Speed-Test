56ba4704c5cf644e3725f10a57a99f7a2fe50c28
ValienteRitter <tagizade2006f@gmail.com>
2026-03-08 20:03:54 +0400

    Style: Style results for bigger displays

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -13,9 +13,11 @@ export function Results({wpm, accuracy, correctTypes, falseTypes}: ResultsProps)
             <img src="src/assets/images/icon-completed.svg" />
             <h1 className="font-bold text-3xl text-center">Test Complete!</h1>
             <p className="text-center text-[hsl(240,3%,46%)]">Solid run. Keep pushing to beat your high score</p>
-            <ResultCard label="WPM" value={wpm.toFixed(2)} color="hsl(0,0%,100%)"/>
-            <ResultCard label="Accuracy" value={accuracy.toFixed(2) + '%'} color="hsl(354,63%,57%)"/>
-            <ResultCard label="Characters" value={correctTypes + '/' + falseTypes} color="hsl(140,63%,57%)"/>
+            <div className="flex flex-col gap-5 w-full sm:flex-row sm:w-[50%] stretch">
+                <ResultCard label="WPM" value={wpm.toFixed(2)} color="hsl(0,0%,100%)"/>
+                <ResultCard label="Accuracy" value={accuracy.toFixed(2) + '%'} color="hsl(354,63%,57%)"/>
+                <ResultCard label="Characters" value={correctTypes + '/' + falseTypes} color="hsl(140,63%,57%)"/>
+            </div>
         </div>
     );
 }
